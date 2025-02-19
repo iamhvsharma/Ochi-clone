b2dc6838e99b0f43d21f8dce517b8ec46935abb4
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-02-20 01:31:55 +0530

    Microelement added Landing complete

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -7,12 +7,17 @@ const LandingPage = () => {
         {["We Create", "Eye Opening", "Presentations"].map((item, index) => {
           return (
             <div className="masker">
+              <div className="flex w-fit overflow-hidden items-end">
+                {index === 1 && (
+                  <div className="w-[6vw] h-[5vw] -top-[1vw] bg-green-500  relative rounded-md mr-[1vw]"> </div>
+                )}
               <h1
                 key={index}
-                className="uppercase text-[6vw] leading-[6vw] font-medium tracking-tighter"
+                className="pt-[2] uppercase text-[7vw] leading-[1] font-semibold tracking-tighter"
               >
                 {item}
               </h1>
+              </div>
             </div>
           );
         })}
@@ -35,7 +40,7 @@ const LandingPage = () => {
           <div className="uppercase px-4 py-2 border-[2px] border-zinc-500 font-light text-sm rounded-full">
             Start the project
           </div>
-          <div className="arrow w-10 h-10 rounded-full border-[1px] flex items-center justify-center">
+          <div className="arrow w-10 h-10 rounded-full border-[2px]  border-zinc-500 flex items-center justify-center">
             <ArrowUpRight />
           </div>
         </div>

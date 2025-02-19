73deae0730708866b49d4c5705c93242b14ecd9f
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-02-20 01:59:27 +0530

    Marque structure & design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import LandingPage from "./components/LandingPage";
+import Marque from "./components/Marque";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="w-full font-['Poppins'] h-screen bg-zinc-900 text-white">
+    <div className="w-full font-['Poppins'] min-h-screen bg-zinc-900 text-white">
       <Navbar />
       <LandingPage />
+      <Marque />
+
     </div>
   );
 };

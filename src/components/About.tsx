395abef40d5df46b595d125d0d032ebf11df5549
Harshvardhan Sharma <itshvsharma@gmail.com>
2025-02-20 10:38:08 +0530

    About section heading

---
+++ src/components/About.tsx
@@ -0,0 +1,13 @@
+
+
+
+const About = () => {
+  return (
+    <div className="w-full p-20 bg-[#cdeb69] rounded-t-3xl text-black">
+        <h1 className="text-[3.2vw] leading-[4vw] ">Ochi is a strategic presentation agency for forward-thinking businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
+        
+    </div>
+  )
+}
+
+export default About
